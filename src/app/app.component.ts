c07cde0946ecc30df9a3f7912b56e2cae0944095
adrianomisina <adriano.misina@gmail.com>
2024-04-10 09:07:25 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'projeto-element-ref';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('minhaDiv') divEl!: ElementRef<HTMLDivElement>
+
+  constructor(private readonly _elRef: ElementRef) {}
+
+  ngOnInit()  {
+    console.log(this._elRef)
+
+    const divEl = this._elRef.nativeElement.querySelector('#minha-outra-div') as HTMLDivElement;
+
+    console.log(divEl)
+
+    divEl.textContent = "Sou uma outra div";
+    divEl.style.backgroundColor = 'blue';
+    divEl.style.color = 'white';
+
+    divEl.addEventListener('click', () => {
+      console.log('Cliquei na div')
+    })
+   }
+
+  ngAfterViewInit() {
+    this.divEl.nativeElement.style.backgroundColor = 'orange';
+    this.divEl.nativeElement.textContent = 'Minha Div';
+    this.divEl.nativeElement.classList.add('minha-classe');
+  }
+
 }
+
+
